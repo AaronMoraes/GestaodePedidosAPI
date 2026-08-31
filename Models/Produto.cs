d2a41fc2f595d9c552663c141b653064ebfd8bf3
Aaron Moraes <147651798+AaronMoraes@users.noreply.github.com>
2026-08-31 08:56:10 -0300

    adiciona build da API com Github Actions

---
+++ Models/Produto.cs
@@ -0,0 +1,8 @@
+namespace GestaodePedidosAPI.Models;
+
+public class Produto
+{
+    public int Id {get; set;}
+    public string Nome {get; set;} = string.Empty;
+    public decimal Preco {get; set;}
+}
--- Program.cs
+++ Program.cs
@@ -1,7 +1,37 @@
+using GestaodePedidosAPI.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 var app = builder.Build();
 
-app.MapGet("/", () => "Gestão de Pedidos API funcionando! ");
+app.MapGet("/api/produtos", () => 
+{
+    var produtos = new List<Produto>
+    {
+        new Produto
+        {
+            Id = 1,
+            Nome = "Coca-Cola 350ml",
+            Preco = 6.00m
+        },
+        new Produto
+        {
+            Id = 2,
+            Nome = "Guarana Antartica 350ml",
+            Preco = 5.00m
+        },
+        new Produto
+        {
+            Id = 3,
+            Nome = "Suco de Laranja 500ml",
+            Preco = 10.00m
+        }
+    };
+
+    return produtos;
+
+});
+
+app.MapGet("/", () => "Gestão de Pedidos API funcionando!");
 
 app.Run();

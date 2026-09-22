37e981889659866ad8ee480cffd1e09e1dfa2761
Aaron Moraes <147651798+AaronMoraes@users.noreply.github.com>
2026-09-22 17:30:48 -0300

    Aplica migrations ao iniciar a API

--- Program.cs
+++ Program.cs
@@ -27,6 +27,12 @@ builder.Services.AddScoped<PedidoService>();
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();
+}
+
 app.UseMiddleware<ExceptionMiddleware>();
 
 app.UseCors("Frontend");
